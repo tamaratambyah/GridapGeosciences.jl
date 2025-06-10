c20db9e933761092d22786b445bb84825cbc67d2
Tamara Tambyah <tamara.tambyah@monash.edu>
2025-06-11 08:57:40 +1000

    manufactured solutions with periodic boundary conditions

--- 2D_refactor_v5/tests/MWE/poisson_convergence_periodic_BC.jl
+++ 2D_refactor_v5/tests/MWE/poisson_convergence_periodic_BC.jl
@@ -14,8 +14,12 @@ Examples are:
       * this function is zero on x=0, x=1, and non-zero (periodic) on y=0, y=1
       * this function is NOT zero-mean
   u_ex = cos(2πx)
-      * this function is periodic
-      * this function is zero-mean
+      * this function is periodic and zero-mean
+      * this function is NOT in the FE space
+  u_ex = ( x(0.5-x)     ; 0.0 < x < 0.5
+           (x-0.5)(x-1) ; 0.5 < x < 1.0    )
+      * this function is periodic and zero-mean
+      * this function is in the FE space (use to manufacture solutions)
 
 To stop the error blowing up, need the enforce zero mean constraint in the FE
 space. This removes the zero eigenvalue which was making the linear system
@@ -25,8 +29,10 @@ Test 2 ways of enforcing zero mean
   2. algebraically via lagrange multipler
 Find that both methods give the same convergence results (expected)
 Find convergence only for zeromean functions (i.e. trig functions)
-Trying to force zero mean to a non zeromean function does not yield good
-convergence results
+  - Trying to force zero mean to a non zeromean function does not yield good
+    convergence results.
+To manufacture solutions in FE space, consider a piecewise polynomial that has
+zero mean (i.e. 'looks like a trig function')
 """
 
 using Gridap, Gridap.Geometry
@@ -99,20 +105,25 @@ function solve_poisson_periodic_lagrange(domain,n,p,degree,uex)
   return e
 end
 
-function plot_error(ns,errs,leginf;ls=:solid)
+function plot_error(ns,errs,leginf;
+    ls=[:solid, :dash, :dot, :dashdot, :dashdotdot],
+    colors = palette(:tab10),
+    markers = [:circle, :rect, :diamond, :utriangle, :cross, :xcross] )
+
   nsims = Int(length(errs)/length(ns))
   for i in 1:nsims
     idx1 = 1 + (i-1)*length(ns)
     idx2 = (i)*length(ns)
     plot!(ns,
           errs[idx1:idx2],
-          lw=3,ls=ls,
-    label=leginf[i])
+          lw=3,
+          c=colors[i],ls=ls[i], markershape=markers[i],
+          label=leginf[i])
   end
 
 end
 
-#### Analytic solution -
+#### Analytic solution with zero mean
 p = 2
 degree = 2*(p+1)
 ns = [2^i for i = 2:6]
@@ -138,13 +149,41 @@ for (key, val) in dd
 end
 
 plot()
-plot_error(ns,errs,collect(keys(dd)))
-plot_error(ns,errs_lagrange,collect(keys(dd));ls=:dash)
+plot_error(ns,errs,collect(keys(dd));ls=fill(:solid,length(dd)))
+plot_error(ns,errs_lagrange,collect(keys(dd));ls=fill(:dashdotdot,length(dd)))
 plot!(xscale=:log10,yscale=:log10,framestyle=:box,
 xlabel="n cells",ylabel="L2(u - u_h)"
 )
 plot!(ns,5e-1dx.^6,lw=2,c=:black,ls=:dash,label="dx^6")
 savefig(plotsdir()*"/poisson_convergence_periodic")
+
+################# piecewise zero mean polynomial function
+function uex(x)
+  if x[1] < 0.5
+    return x[1]*(0.5-x[1])
+  else
+    return (x[1]-0.5)*(x[1]-1.0)
+  end
+end
+
+errs = []
+errs_lagrange = []
+for n in collect(ns)
+  e = solve_poisson_periodic((0,1,0,1),(n,n),p,degree,uex)
+  eg = solve_poisson_periodic_lagrange((0,1,0,1),(n,n),p,degree,uex)
+  push!(errs,e)
+  push!(errs_lagrange,eg)
+end
+
+
+plot()
+plot_error(ns,errs,["constraint"])
+plot_error(ns,errs_lagrange,["lagrange"];ls=[:dash])
+plot!(xscale=:log10,yscale=:log10,framestyle=:box,
+xlabel="n cells",ylabel="L2(u - u_h)"
+)
+savefig(plotsdir()*"/poisson_manufactured_periodic")
+
 
 
 ################# force zero-mean into non zero mean analytic function
@@ -166,7 +205,7 @@ for n in collect(ns)
 end
 plot()
 plot_error(ns,errs,["constraint"])
-plot_error(ns,errs,["lagrange"])
+plot_error(ns,errs_lagrange,["lagrange"];ls=[:dash])
 plot!(xscale=:log10,yscale=:log10,framestyle=:box,
 xlabel="n cells",ylabel="L2(u - u_h)"
 )
@@ -177,8 +216,18 @@ savefig(plotsdir()*"/poisson_convergence_periodic_poly_force")
 
 
 ################# low level
-u(x) =  x[1]*(1-x[1]) #cos(2*π*x[1])*cos(2*π*x[2])
-_u(x) = u(x) - sum(∫( u )dΩ)
+# u(x) =  x[1]*(1-x[1]) #cos(2*π*x[1])*cos(2*π*x[2])
+# _u(x) = u(x) - sum(∫( u )dΩ)
+degree = 2
+p = 2
+function u(x)
+  if x[1] < 0.5
+    return x[1]*(0.5-x[1])
+  else
+    return (x[1]-0.5)*(x[1]-1.0)
+  end
+end
+
 
 rhs(x) = -1.0*(laplacian(u)(x))
 
@@ -190,8 +239,10 @@ model = CartesianDiscreteModel((0,1, 0,1), (12,12), isperiodic=(true,true))
 Ω = Triangulation(model)
 dΩ = Measure(Ω,degree)
 
+sum(∫(u)dΩ  )
+
 writevtk(Ω,dir*"/poisson_manufactured_periodic_BC",
-        cellfields=["u"=>u, "u0"=>_u],append=false)
+        cellfields=["u"=>u],append=false)
 
 ## assess zero mean of analytic function
 sum(∫(u)dΩ)
@@ -210,3 +261,6 @@ sum( ∫(uh )dΩ  )
 #### Compute errors
 e = uh-u
 sum(∫(e⊙e)dΩ)
+
+writevtk(Ω,dir*"/poisson_manufactured_periodic_BC",
+        cellfields=["u"=>u,"uh"=>uh,"e"=>e],append=false)
