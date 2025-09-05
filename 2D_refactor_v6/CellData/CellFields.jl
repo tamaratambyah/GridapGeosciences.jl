53cbbece9f4cb35da2c4f5438d328268c7c4a52b
Tamara Tambyah <tamara.tambyah@monash.edu>
2025-09-05 15:35:04 +1000

    save work

--- 2D_refactor_v6/CellData/CellFields.jl
+++ 2D_refactor_v6/CellData/CellFields.jl
@@ -3,6 +3,19 @@ function panelwise_cellfield(f::Function,trian::Triangulation,panel_ids::Abstrac
   CellData.GenericCellField(cell_field,trian,PhysicalDomain())
 end
 
+
+
+function panelwise_cellfield(f::Function,atrian::AdaptedTriangulation,panel_ids::SkeletonPair{<:AbstractArray{Int}})
+  panelwise_cellfield(f,atrian.trian,panel_ids)
+end
+
+function panelwise_cellfield(f::Function,trian::Triangulation,skel_panel_ids::SkeletonPair{<:AbstractArray{Int}})
+  plus = panelwise_cellfield(f, trian.plus, skel_panel_ids.plus)
+  minus = panelwise_cellfield(f, trian.minus, skel_panel_ids.minus)
+  SkeletonPair(plus,minus)
+end
+
+
 function ambient_cellfield(panel_cf::CellField,ambient_trian::Triangulation,panel_ids::AbstractArray{Int})
   inv_f = lazy_map(p->InverseMap(p),panel_ids)
   _cf = change_domain(panel_cf,DomainStyle(panel_cf),PhysicalDomain())
--- 2D_refactor_v6/Tests/skeleton.jl
+++ 2D_refactor_v6/Tests/skeleton.jl
@@ -9,6 +9,7 @@ function Geometry.BoundaryTriangulation(model::ParametricDiscreteModel,lcell=1)
   Geometry.BoundaryTriangulation(model,face_to_bgface,bgface_to_lcell)
 end
 
+
 function Geometry.BoundaryTriangulation(
   model::ParametricDiscreteModel,
   bgface_to_mask::AbstractVector{Bool},
@@ -23,6 +24,7 @@ function Geometry.BoundaryTriangulation(
   face_to_bgface::AbstractVector{<:Integer},
   bgface_to_lcell::AbstractVector{<:Integer}
   )
+  println("my boundary trian")
 
   topo = get_grid_topology(model)
   Dc = num_cell_dims(model)
@@ -70,18 +72,18 @@ function Geometry.BoundaryTriangulation(
 
 end
 
+function get_panel_ids(atrian::AdaptedTriangulation)
+  get_panel_ids(atrian.trian)
+end
 
-panel_model = coarse_parametric_model()
-panel_model = Adaptivity.refine(panel_model)
-btrian = BoundaryTriangulation(panel_model,2)
+function get_panel_ids(btrian::BoundaryTriangulation)
+  get_face_panel_ids(btrian)
+end
 
-
-face_panel_ids = get_face_panel_ids(btrian)
-face_geo_map = lazy_map(p -> MatMultField(R1p[p]) ∘ ForwardMapPanel1(), face_panel_ids)
-writevtk(btrian,dir*"/ambient_model_skeleton",append=false,geo_map=face_geo_map)
-
-function get_face_panel_ids(atrian::AdaptedTriangulation)
-  get_face_panel_ids(atrian.trian)
+function get_panel_ids(strian::SkeletonTriangulation)
+  plus = get_face_panel_ids(strian.plus)
+  minus = get_face_panel_ids(strian.plus)
+  SkeletonPair(plus,minus)
 end
 
 function get_face_panel_ids(btrian::BoundaryTriangulation)
@@ -97,53 +99,125 @@ end
 
 
 
-### adapted model
+
+
+panel_model = coarse_parametric_model()
 panel_model = Adaptivity.refine(panel_model)
-panel_ids = get_panel_ids(panel_model)
-btrian = BoundaryTriangulation(panel_model,2)
 
-glue = get_glue(btrian,Val(2))
-face_2_cell = glue.tface_to_mface
-face_panel_ids = panel_ids[face_2_cell]
-face_geo_map = lazy_map(p -> MatMultField(R1p[p]) ∘ ForwardMapPanel1(), face_panel_ids)
-writevtk(btrian,dir*"/ambient_model_skeleton",append=false,geo_map=face_geo_map)
-
-
-
-### adapted, adapted model
-panel_model = Adaptivity.refine(panel_model)
-panel_ids = get_panel_ids(panel_model)
 btrian = BoundaryTriangulation(panel_model,1)
 
-glue = get_glue(btrian,Val(2))
-face_2_cell = glue.tface_to_mface
-face_panel_ids = panel_ids[face_2_cell]
+############ left and right normals
+## left
+btrian = BoundaryTriangulation(panel_model,1)
+n = get_normal_vector(btrian)
+face_panel_ids = get_panel_ids(btrian)
+
+jacobian_cf = panelwise_cellfield(forward_jacobian,btrian,face_panel_ids)
+inv_metric_cf = CellField(analytic_inv_metric,btrian)
+
+_n_mapped = jacobian_cf ⋅ (inv_metric_cf ⋅ n)
+ff = Operation(sqrt)(  n ⋅ (inv_metric_cf ⋅ n)  )
+n_mapped = _n_mapped/ff
+
+
+panel_cfs = [jacobian_cf, inv_metric_cf, n , n_mapped]
+labels = ["jacobian","metric","n", "n_mapped"]
+cellfields = map((x,y) -> x=>y, labels,panel_cfs)
+
 face_geo_map = lazy_map(p -> MatMultField(R1p[p]) ∘ ForwardMapPanel1(), face_panel_ids)
+writevtk(btrian,dir*"/ambient_model_skeleton_left",cellfields=cellfields,append=false,geo_map=face_geo_map)
 
-writevtk(btrian,dir*"/ambient_model_skeleton",append=false,geo_map=face_geo_map)
+## right
+btrian = BoundaryTriangulation(panel_model,2)
+n = get_normal_vector(btrian)
+face_panel_ids = get_panel_ids(btrian)
+
+jacobian_cf = panelwise_cellfield(forward_jacobian,btrian,face_panel_ids)
+inv_metric_cf = CellField(analytic_inv_metric,btrian)
+
+_n_mapped = jacobian_cf ⋅ (inv_metric_cf ⋅ n)
+ff = Operation(sqrt)(  n ⋅ (inv_metric_cf ⋅ n)  )
+n_mapped = _n_mapped/ff
 
 
+panel_cfs = [jacobian_cf, inv_metric_cf, n , n_mapped]
+labels = ["jacobian","metric","n", "n_mapped"]
+cellfields = map((x,y) -> x=>y, labels,panel_cfs)
 
-_skel_trian = SkeletonTriangulation(panel_model)
-
-skel_trian = _skel_trian.trian
-left_btrian = skel_trian.minus
-glue = get_glue(left_btrian,Val(2))
-face_2_cell = glue.tface_to_mface
-face_panel_ids = panel_ids[face_2_cell]
 face_geo_map = lazy_map(p -> MatMultField(R1p[p]) ∘ ForwardMapPanel1(), face_panel_ids)
-writevtk(left_btrian,dir*"/ambient_model_skeleton_minus",append=false,geo_map=face_geo_map)
+writevtk(btrian,dir*"/ambient_model_skeleton_right",cellfields=cellfields,append=false,geo_map=face_geo_map)
 
 
-right_btrian = skel_trian.plus
-glue = get_glue(right_btrian,Val(2))
-face_2_cell = glue.tface_to_mface
-face_panel_ids = panel_ids[face_2_cell]
-face_geo_map = lazy_map(p -> MatMultField(R1p[p]) ∘ ForwardMapPanel1(), face_panel_ids)
-writevtk(right_btrian,dir*"/ambient_model_skeleton_plus",append=false,geo_map=face_geo_map)
+######## skeleton
+
+panel_model = coarse_parametric_model()
+panel_model = Adaptivity.refine(panel_model)
+
+skel_trian = SkeletonTriangulation(panel_model)
+n_skel = get_normal_vector(skel_trian)
+skel_panel_ids = get_panel_ids(skel_trian)
+
+panel_cfs = [n_skel.plus , n_skel.minus]
+labels = ["n_plus", "n_mins"]
+cellfields = map((x,y) -> x=>y, labels,panel_cfs)
+skel_geo_map = lazy_map(p -> MatMultField(R1p[p]) ∘ ForwardMapPanel1(), skel_panel_ids.plus)
+writevtk(skel_trian,dir*"/ambient_model_skeleton",cellfields=cellfields,append=false,geo_map=skel_geo_map)
+
+
+jacobian_cf = panelwise_cellfield(forward_jacobian,skel_trian,skel_panel_ids)
+inv_metric_cf = CellField(analytic_inv_metric,skel_trian)
+
+inv_metric_cf ⋅n_skel
+
+skel_trian = SkeletonTriangulation(panel_model)
+n_skel = get_normal_vector(skel_trian)
+n_skel⋅n_skel
+n_skel.plus ⋅ n_skel.plus
+
+cache = nothing
+evaluate!(cache,⋅,n_skel.plus,n_skel.plus)
+evaluate!(cache,⋅,n_skel,n_skel)
 
 
 
+jacobian_cf ⋅n_skel
 
-visdata, = visualization_data(skel_trian,dir*"/ambient_model_skeleton")
-get_cell_coordinates(visdata.grid)
+_n_mapped = jacobian_cf ⋅ (inv_metric_cf ⋅ n_skel)
+ff = Operation(sqrt)(  n ⋅ (inv_metric_cf ⋅ n_skel)  )
+n_mapped = _n_mapped/ff
+
+############## only 1 panel
+skel_trian = SkeletonTriangulation(panel_model)
+_skel_panel_ids = get_panel_ids(skel_trian)
+
+
+face_to_mask = collect(_skel_panel_ids.plus.==1)
+skel_trian = SkeletonTriangulation(panel_model,face_to_mask)
+
+n_skel = get_normal_vector(skel_trian)
+skel_panel_ids = get_panel_ids(skel_trian)
+
+
+jacobian_cf = panelwise_cellfield(forward_jacobian,skel_trian,skel_panel_ids)
+inv_metric_cf = CellField(analytic_inv_metric,skel_trian)
+
+_n_mapped = jacobian_cf.plus ⋅ (inv_metric_cf ⋅ n_skel.plus)
+ff = Operation(sqrt)(  n_skel.plus ⋅ (inv_metric_cf ⋅ n_skel.plus)  )
+n_mapped = _n_mapped/ff
+
+_n_mapped = jacobian_cf.minus ⋅ (inv_metric_cf ⋅ n_skel.minus)
+ff = Operation(sqrt)(  n_skel.minus ⋅ (inv_metric_cf ⋅ n_skel.minus)  )
+n_mapped_minus = _n_mapped/ff
+
+panel_cfs = [n_mapped, n_mapped_minus]
+labels = ["n_plus", "n_minus"]
+cellfields = map((x,y) -> x=>y, labels,panel_cfs)
+skel_geo_map = lazy_map(p -> MatMultField(R1p[p]) ∘ ForwardMapPanel1(), skel_panel_ids.plus)
+writevtk(skel_trian,dir*"/ambient_model_skeleton",cellfields=cellfields,append=false,geo_map=skel_geo_map)
+
+
+panel_cfs = [n_skel.plus, n_skel.minus]
+labels = ["n_plus", "n_minus"]
+cellfields = map((x,y) -> x=>y, labels,panel_cfs)
+skel_geo_map = lazy_map(p -> MatMultField(R1p[p]) ∘ ForwardMapPanel1(), skel_panel_ids.plus)
+writevtk(skel_trian,dir*"/ambient_model_skeleton_flat",cellfields=cellfields,append=false)
