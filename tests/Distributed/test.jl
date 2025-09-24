8e4234d4b1e3924f0259380a93b8a8d6e93f8809
Tamara Tambyah <tamara.tambyah@monash.edu>
2025-09-24 11:43:25 +1000

    working on distributed visualiation

--- tests/Distributed/test.jl
+++ tests/Distributed/test.jl
@@ -32,12 +32,12 @@ writevtk(Triangulation(serial_parent),dir*"/ambient_model",geo_map=serial_cell_g
 
 
 
-nprocs = (2,2)
+nprocs = (1,2)
 ranks  = with_debug() do distribute
   distribute(LinearIndices((prod(nprocs),)))
 end
 
-parent_cell_to_part = [1,1,2,2,3,4]
+parent_cell_to_part = [1,1,1,2,2,2]
 child_cell_to_part  = lazy_map(Reindex(parent_cell_to_part),serial_rglue.n2o_faces_map[3])
 println(child_cell_to_part)
 if GridapDistributed.i_am_in(ranks)
@@ -73,6 +73,7 @@ cell_geo_map = map(panel_ids) do pid
   return lazy_map(p -> MatMultField(R1p[p]) ∘ ForwardMapPanel1(), pid)
 end
 
+include("vtk.jl")
 writevtk(Triangulation(parent),dir*"/ambient_model",geo_map=cell_geo_map)
 
 # map(local_views(model_ref)) do model
@@ -83,108 +84,6 @@ writevtk(Triangulation(parent),dir*"/ambient_model",geo_map=cell_geo_map)
 cellx = get_cell_coordinates(trian)
   println(cellx)
   cellx_mapped = lazy_map(evaluate,geo_map,cellx)
-
-function Gridap.Visualization.write_vtk_file(
-  parts::AbstractArray,
-  grid::AbstractArray{<:Gridap.Geometry.Grid}, filebase; celldata, nodaldata,
-  geo_map,
-  compress=false,append=true,ascii=false,vtkversion=:default
-  )
-  pvtk = Gridap.Visualization.create_vtk_file(
-    parts,grid,filebase;celldata=celldata,nodaldata=nodaldata,
-    geo_map=geo_map,
-    compress=compress,append=append,ascii=ascii,vtkversion=vtkversion
-  )
-  map(Gridap.Visualization.vtk_save,pvtk)
-end
-
-function Gridap.Visualization.create_vtk_file(
-  parts::AbstractArray,
-  grid::AbstractArray{<:Gridap.Geometry.Grid},
-  filebase;
-  celldata, nodaldata,
-  geo_map,
-  compress=false,append=true,ascii=false,vtkversion=:default
-)
-  nparts = length(parts)
-  map(parts,grid,celldata,nodaldata,geo_map) do part,g,c,n,gm
-    Gridap.Visualization.create_pvtk_file(
-      g,filebase;
-      part=part,nparts=nparts,
-      celldata=c,nodaldata=n,
-      geo_map=gm,
-      compress=compress,append=append,ascii=ascii,vtkversion=vtkversion
-    )
-  end
-end
-
-function Gridap.Visualization.writevtk(
-  arg::GridapDistributed.DistributedModelOrTriangulation,args...;
-  geo_map=nothing,
-  compress=false,append=true,ascii=false,vtkversion=:default,kwargs...
-)
-  parts=get_parts(arg)
-  map(GridapDistributed.visualization_data(arg,args...;kwargs...)) do visdata
-    Gridap.Visualization.write_vtk_file(
-      parts,visdata.grid,visdata.filebase,celldata=visdata.celldata,nodaldata=visdata.nodaldata,
-      geo_map=geo_map,
-      compress=compress, append=append, ascii=ascii, vtkversion=vtkversion
-    )
-  end
-end
-
-function Gridap.Visualization.createvtk(
-  arg::GridapDistributed.DistributedModelOrTriangulation,args...;
-  geo_map=nothing,
-  compress=false,append=true,ascii=false,vtkversion=:default,kwargs...
-)
-  v = visualization_data(arg,args...;kwargs...)
-  parts=get_parts(arg)
-  @Gridap.Helpers.notimplementedif length(v) != 1
-  visdata = first(v)
-  Gridap.Visualization.create_vtk_file(
-    parts,visdata.grid,visdata.filebase,celldata=visdata.celldata,nodaldata=visdata.nodaldata,
-    geo_map=geo_map,
-    compress=compress, append=append, ascii=ascii, vtkversion=vtkversion
-  )
-end
-
-function Gridap.Visualization.create_pvtk_file(
-  trian::Gridap.Geometry.Grid, filebase; part, nparts, ismain=(part==1), celldata=Dict(), nodaldata=Dict(),
-  geo_map=nothing,
-  compress=false, append=true, ascii=false, vtkversion=:default
-)
-  println("my distributed vis")
-
-
-  # compute the regular visualisation points
-  points = Gridap.Visualization._vtkpoints(trian)
-
-  println(typeof(geo_map)<:AbstractArray)
-  ## if geo_map provided, map the points to ambient space
-  if geo_map != nothing
-    points = mapped_vtkpoints(trian,geo_map)
-  end
-
-  cells = Gridap.Visualization._vtkcells(trian)
-  vtkfile = Gridap.Visualization.pvtk_grid(
-    filebase, points, cells;part=part, nparts=nparts, ismain=ismain,
-    compress=compress, append=append, ascii=ascii, vtkversion=vtkversion
-  )
-
-  if num_cells(trian) > 0
-    for (k, v) in celldata
-      # component_names are actually always nothing as there are no field in ptvk atm
-      component_names = Gridap.Visualization._data_component_names(v)
-      vtkfile[k, Gridap.Visualization.VTKCellData(), component_names=component_names] = Gridap.Visualization._prepare_data(v)
-    end
-    for (k, v) in nodaldata
-      component_names = Gridap.Visualization._data_component_names(v)
-      vtkfile[k, Gridap.Visualization.VTKPointData(), component_names=component_names] = Gridap.Visualization._prepare_data(v)
-    end
-  end
-  return vtkfile
-end
 
 
 
