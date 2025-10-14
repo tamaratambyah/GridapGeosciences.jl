d2b013cd3820453ae46bad98b814801f84445995
Tamara Tambyah <tamara.tambyah@monash.edu>
2025-10-14 15:44:41 +1100

    fixing panel ids tests in Distributed

--- tests/Distributed/DistributedPanelIdsTests.jl
+++ tests/Distributed/DistributedPanelIdsTests.jl
@@ -3,7 +3,7 @@ using Gridap
 using GridapGeosciences
 using Gridap.Adaptivity
 using Test
-
+using GridapDistributed
 # using Test; @testset "DistributedPanelIds" begin include("tests/Distributed/DistributedPanelIdsTests.jl") end
 
 include("../convergence_tools.jl")
@@ -23,14 +23,16 @@ dmodels = get_distributed_refined_models(ranks,nprocs,n_ref_lvls)
 
 function test_distributed_panel_ids(dpanel_model)
   trian = Triangulation(dpanel_model)
-
+  gids = get_cell_gids(dpanel_model)
   map(get_panel_ids(dpanel_model), get_panel_ids(trian), local_views(dpanel_model)) do p1, p2, model
     @test p1 == p2
     @test length(p1) == num_cells(model)
   end
 
-  map(get_owned_panel_ids(dpanel_model),get_owned_panel_ids(trian)) do p1, p2
-    @test p1 == p2
+  map(get_owned_panel_ids(dpanel_model),get_panel_ids(trian),partition(gids)) do p1, p2, cid
+    owned_cells = own_to_local(cid)
+    _p2 = p2[owned_cells]
+    @test p1 == _p2
   end
 
 end
