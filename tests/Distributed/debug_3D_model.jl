e213cce041ccd7fddf07bf30708729bcb0d9e238
Tamara Tambyah <tamara.tambyah@monash.edu>
2025-11-04 13:57:34 +1100

    tidying plotting

--- tests/Distributed/debug_3D_model.jl
+++ tests/Distributed/debug_3D_model.jl
@@ -7,14 +7,15 @@ using Gridap
 using GridapDistributed
 
 using DrWatson
-dir = datadir("Distributed")
-!isdir(dir) && mkdir(dir)
 
 MPI.Init()
 ranks = distribute_with_mpi(LinearIndices((prod(MPI.Comm_size(MPI.COMM_WORLD)),)))
 
-num_horizontal_uniform_refinements = 1
-num_vertical_uniform_refinements = 1
+dir = datadir("Distributed")
+(i_am_main(ranks) && !isdir(dir)) && mkdir(dir)
+
+num_horizontal_uniform_refinements = 3
+num_vertical_uniform_refinements = 2
 o3model = GridapGeosciences.Distributed.Parametric3DOctreeDistributedDiscreteModel(ranks;
                                            num_horizontal_uniform_refinements=num_horizontal_uniform_refinements,
                                            num_vertical_uniform_refinements=num_vertical_uniform_refinements);
@@ -31,30 +32,31 @@ writevtk(Ω_panel,dir*"/extruded_model",append=false,geo_map=cell_geo_map)
 writevtk(Ω_panel,dir*"/extruded_model",append=false)
 
 
-##### testing boundary tags
-labels = panel_model.models.item.face_labeling
-labs = Gridap.Geometry.get_tag_name(labels)
-
-
-
-# tags = ["intermediate_boundary",  "bottom_boundary",  "top_boundary"]
+################################################################################
+#### Plot the boundaries
+#### tags = ["intermediate_boundary",  "bottom_boundary",  "top_boundary"]
+#### Need to call cell_geo_map map with the panel_ids of Γ
+################################################################################
 tags = ["bottom_boundary"]
 Γ = BoundaryTriangulation(panel_model,tags=tags)
-Γ.trians.item_ref[].parent.glue.face_to_bgface
-cell_geo_map = geo_map_func(Γ)
+cell_geo_map = geo_map_func(get_panel_ids(Γ))
 writevtk(Γ,dir*"/boundary_bottom",append=false,geo_map=cell_geo_map)
 
 tags = ["top_boundary"]
 Γ = BoundaryTriangulation(panel_model,tags=tags)
 Γ.trians.item_ref[].parent.glue.face_to_bgface
-cell_geo_map = geo_map_func(Γ)
+cell_geo_map = geo_map_func(get_panel_ids(Γ))
 writevtk(Γ,dir*"/boundary_top",append=false,geo_map=cell_geo_map)
 
 tags = ["intermediate_boundary"]
 Γ = BoundaryTriangulation(panel_model,tags=tags)
-cell_geo_map = geo_map_func(Γ)
-writevtk(Γ,dir*"/boundary_intermediate",append=false)#,geo_map=cell_geo_map)
+cell_geo_map = geo_map_func(get_panel_ids(Γ))
+writevtk(Γ,dir*"/boundary_intermediate",append=false,geo_map=cell_geo_map)
 
+
+################################################################################
+#### Vertical refinement
+################################################################################
 o3model = GridapGeosciences.Distributed.vertically_uniformly_refine(o3model)
 panel_model = o3model.parametric_dmodel
 
@@ -72,7 +74,9 @@ map(local_views(panel_model)) do model
     writevtk(Ω_panel,dir*"/vrefined_extruded_model",append=false,geo_map=cell_geo_map)
 end
 
-
+################################################################################
+#### Horizontal refinement
+################################################################################
 o3model = GridapGeosciences.Distributed.horizontally_uniformly_refine(o3model)
 panel_model = o3model.parametric_dmodel
 map(local_views(panel_model)) do model
