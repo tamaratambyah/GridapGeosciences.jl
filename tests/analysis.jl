42fa23cccc49064b4185017415cb84c7dda41752
Tamara Tambyah <tamara.tambyah@monash.edu>
2025-11-10 12:06:50 +1100

    tidying wave test, remove unnecessary files

--- tests/analysis.jl
+++ tests/analysis.jl
@@ -115,7 +115,7 @@ using DrWatson
 using DataFrames
 # using GridapGeosciences
 include("convergence_tools.jl")
-dir = datadir("Wave3D/convergence")
+dir = datadir("WaveConvergence_3D/func_z1/convergence")
 df = collect_results(dir)
 
 ps = unique(df.p_fe)
