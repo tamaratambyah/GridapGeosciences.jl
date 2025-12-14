dfa62fc815af7c80e53b0b11fbecf0777f8c51d4
Tamara Tambyah <tamara.tambyah@monash.edu>
2025-12-15 09:29:26 +1100

    testing checkpointing in distributed

--- tests/Distributed/mwe.jl
+++ tests/Distributed/mwe.jl
@@ -7,94 +7,91 @@ using GridapGeosciences
 using MPI
 using PartitionedArrays
 
+include("../convergence_tools.jl")
 
-MPI.Init()
-ranks = distribute_with_mpi(LinearIndices((prod(MPI.Comm_size(MPI.COMM_WORLD)),)))
+nprocs = 2
+ranks = with_debug() do distribute
+  distribute(LinearIndices((nprocs,)))
+end
 
-include("../Geophysical/Williamson2Test.jl")
+## initial conditions
+vecX(XYZ) = zero(XYZ)
+depth(XYZ) = 1.0 + 0.01*exp(-5*((1-XYZ[1])^2+(0-XYZ[2])^2+(0-XYZ[3])^2))
 
-dir = datadir("DistributedWaveEquation")
-(i_am_main(ranks) && !isdir(dir)) && mkdir(dir)
 
-h = panel_to_cartesian(h₀(0.0))
-vX = panel_to_cartesian(tangent_vec(u₀(0.0)))
+h = panel_to_cartesian(depth)
+vX = panel_to_cartesian(tangent_vec(vecX))
+
+models  = get_distributed_refined_models(ranks,nprocs,2)
+panel_model = models[1]
 p_fe = 1
-ls = LUSolver()
 
-omodel = ParametricOctreeDistributedDiscreteModel(ranks; num_initial_uniform_refinements=3)
-panel_model = omodel.parametric_dmodel
-
-# das = FullyAssembledRows()
-das = SubAssembledRows()
-
+## finite element solver
 panel_ids = get_panel_ids(panel_model)
-Ω_panel = Triangulation(das,panel_model)
+Ω_panel = Triangulation(panel_model)
 dΩ = Measure(Ω_panel,2*(p_fe+1))
 
-### See https://github.com/tamaratambyah/GridapGeosciences.jl/issues/5
-### use triangulation for FE space
 Q = TestFESpace(Ω_panel, ReferenceFE(lagrangian,Float64,p_fe); conformity=:L2)
-P = TrialFESpace(Q)
+P = TransientTrialFESpace(Q)
 
 V = TestFESpace(Ω_panel, ReferenceFE(raviart_thomas,Float64,p_fe); conformity=:HDiv)
-U = TrialFESpace(V)
+U = TransientTrialFESpace(V)
 
 Y = MultiFieldFESpace([V, Q])
 X = MultiFieldFESpace([U, P])
 
+## initial conditions
 h_cf = panelwise_cellfield(h,Ω_panel,panel_ids)
-u_proj_cf = panelwise_cellfield(projection_v(vX),Ω_panel,panel_ids)
-u_contra_cf = panelwise_cellfield(contra_v(vX),Ω_panel,panel_ids)
-covarient_basis_cf = panelwise_cellfield(covarient_basis,Ω_panel,panel_ids)
-
-# interpolate into FE space
-xh_interpolate = interpolate([u_contra_cf,h_cf],X)
-u_contra_h = xh_interpolate[1] ## contravariant components
-u_interpolate = covarient_basis_cf⋅u_contra_h ## physical velocity
-h_interpolate = xh_interpolate[2]
-
-sdiv_cf =  panelwise_cellfield(surfdiv(contra_v(vX)),Ω_panel,panel_ids)
-sgrad_cf = panelwise_cellfield(sgrad(h),Ω_panel,panel_ids)
-pinvJ_cf = panelwise_cellfield(forward_pinv_jacobian,Ω_panel,panel_ids)
-
-# manufacture rhs functions
-rhs_scalar = h_cf + sdiv_cf
-rhs_vector = u_proj_cf + sgrad_cf
-rhs_con_vector = pinvJ_cf ⋅ rhs_vector # exact contravariant component
+vec_contra_cf = panelwise_cellfield(contra_v(vX),Ω_panel,panel_ids)
+xh0 = interpolate([vec_contra_cf,h_cf],X)
 
 
+## transient weak form
 metric_cf = panelwise_cellfield(metric,Ω_panel,panel_ids)
 meas_cf = panelwise_cellfield(sqrtg,Ω_panel,panel_ids)
 grad_meas_cf = panelwise_cellfield(grad_meas,Ω_panel,panel_ids)
 
-biform1((u,p),(v,q)) = ∫( (u⋅ (metric_cf⋅v))*meas_cf )dΩ - ∫( p*(v⋅grad_meas_cf + meas_cf*(∇⋅v) ) )dΩ
-biform2((u,p),(v,q)) = ∫( (p*q)*meas_cf )dΩ + ∫( q*(u⋅grad_meas_cf + meas_cf*(∇⋅u) )  )dΩ
+mass(t, (dtu,dtp), (v,q)) = ∫( (v⋅ (metric_cf⋅ dtu) )*meas_cf )dΩ + ∫( (q*dtp)*meas_cf )dΩ
+res(t,(u,p),(v,q)) =  ∫( q*(u⋅grad_meas_cf + meas_cf*(∇⋅u) )  )dΩ - ∫( p*(v⋅grad_meas_cf + meas_cf*(∇⋅v) ) )dΩ
+jac(t,(u,p),(du,dp),(v,q)) =  res(t,(du,dp),(v,q))
+jac_t(t,(u,p),(dtu,dtp),(v,q)) =  mass(t, (dtu,dtp), (v,q))
 
-biformX((u,p),(v,q)) = biform1((u,p),(v,q)) + biform2((u,p),(v,q))
-liformX((v,q)) = ∫( rhs_con_vector⋅(metric_cf⋅v)*meas_cf )dΩ + ∫( (rhs_scalar*q)*meas_cf )dΩ
+opT = TransientSemilinearFEOperator(mass,res,(jac,jac_t),X,Y, constant_mass=true)
 
-assem = SparseMatrixAssembler(X,Y,das)
-op = AffineFEOperator(biformX,liformX,X,Y,assem)
-uh,ph = solve(ls,op)
+# transient parameters
+t0 = 0.0
+dt = 0.01
+tF = 1*dt
 
-uh_proj = covarient_basis_cf ⋅ uh
+# solve with SSP RK 3
+solver = RungeKutta(LUSolver(), LUSolver(), dt, :EXRK_SSP_3_3)
+solT = solve(solver, opT, t0, tF, xh0)
 
-Ω_error = Triangulation(panel_model)
-dΩ_error = Measure(Ω_error,6*p_fe+1)
-e_u = l2( (u_proj_cf - uh_proj),meas_cf,dΩ_error) # error in physical velocity u
-e_p = l2((h_cf - ph),meas_cf,dΩ_error) # error in depth
-
-
-
-_Ω_panel = Triangulation(panel_model)
-cell_geo_map = geo_map_func(_Ω_panel)
-
-if das == FullyAssembledRows()
-  cell_geo_map = geo_map_func(get_panel_ids(_Ω_panel))
+uh = xh0[1]
+ph = xh0[2]
+for (t, xh) in solT
+  uh,ph = xh
+  i_am_main(ranks) && println("t = ", t)
 end
 
-panel_cfs = [h_cf, u_proj_cf, ph, uh_proj, h_cf-ph, u_proj_cf-uh_proj, u_interpolate, h_interpolate ]
-labels = ["p","u_proj", "ph", "uh_proj", "ep","eu", "u_interpolate", "h_interpolate"]
+covarient_basis_cf = panelwise_cellfield(covarient_basis,Ω_panel,panel_ids)
+cell_geo_map = geo_map_func(Ω_panel)
+dir = datadir("TransientTest")
+writevtk(Ω_panel,dir*"/solT_test" * ".vtu", cellfields= ["uh"=>covarient_basis_cf⋅uh, "ph"=>ph],append=false,geo_map=cell_geo_map)
 
-cellfields = map((x,y) -> x=>y, labels,panel_cfs)
-writevtk(Ω_panel,dir*"/sol_proc$(length(ranks))",cellfields=cellfields,append=false,geo_map=cell_geo_map)
+
+## extract free values
+p_vec =  ph.metadata.free_values
+u_vec = uh.metadata.free_values
+
+## save free values to file
+
+
+## load free values from file
+# p_free_vals =
+# u_free_vals =
+
+## interpoalte onto space
+# ph_load = interpolate(p_free_vals, P)
+# uh_load = interpolate(u_free_vals,U)
+# xh_load = interpolate([uh_load,ph_load],X) ## mutlifield
