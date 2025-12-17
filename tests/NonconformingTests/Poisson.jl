98f25f3a34a9fd45b08cdd61f3ac13e92f358625
Tamara Tambyah <tamara.tambyah@monash.edu>
2025-12-17 17:31:06 +1100

    updating poisson test to work with zero mean

--- tests/NonconformingTests/Poisson.jl
+++ tests/NonconformingTests/Poisson.jl
@@ -64,12 +64,20 @@ sum(∫( laplacian(u_ex))dΩ  )
 
 ################################################################################
 ####### use zeromean constraint in FE space
-V = FESpace(model,ReferenceFE(lagrangian,Float64,order);conformity=:H1,constraint=:zeromean)
+V0 = FESpace(model,ReferenceFE(lagrangian,Float64,order);conformity=:H1)
+V = Gridap.FESpaces.ZeroMeanFESpace(V0,dΩ)
 U = TrialFESpace(V)
 
 map(V.spaces) do space
   typeof(space)<: Gridap.FESpaces.FESpaceWithLinearConstraints
 end
+
+Gridap.FESpaces.get_cell_constraints(f::Gridap.FESpaces.FESpaceWithConstantFixed,x::Gridap.FESpaces.Constrained) = Gridap.FESpaces.get_cell_constraints(f.space,x)
+Gridap.FESpaces.get_cell_constraints(f::Gridap.FESpaces.FESpaceWithLinearConstraints,x::Gridap.FESpaces.Constrained) = Gridap.FESpaces.get_cell_constraints(f)
+Gridap.FESpaces.get_cell_isconstrained(f::Gridap.FESpaces.FESpaceWithConstantFixed,x::Gridap.FESpaces.Constrained) = Gridap.FESpaces.get_cell_isconstrained(f.space,x)
+Gridap.FESpaces.get_cell_isconstrained(f::Gridap.FESpaces.FESpaceWithLinearConstraints,x::Gridap.FESpaces.Constrained) = Gridap.FESpaces.get_cell_isconstrained(f)
+
+
 
 biform(u,v) = ∫( ∇(u)⋅∇(v)  )dΩ
 liform(v) = ∫( v*f_ex )dΩ
