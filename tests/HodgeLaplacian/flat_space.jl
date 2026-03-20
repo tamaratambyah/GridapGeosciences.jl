26de0110a118912146d564b36a494abf43d1e48a
Tamara Tambyah <tamara.tambyah@monash.edu>
2026-03-20 18:02:34 +1100

    working hodge laplacian test

--- tests/HodgeLaplacian/flat_space.jl
+++ tests/HodgeLaplacian/flat_space.jl
@@ -1,6 +1,3 @@
-using MPI
-using PartitionedArrays
-
 using DrWatson
 using Gridap
 using GridapDistributed
@@ -10,101 +7,115 @@ using Gridap.Geometry, Gridap.Adaptivity, Gridap.Helpers, Gridap.Algebra
 using Gridap.ReferenceFEs, Gridap.Polynomials, Gridap.CellData
 
 using GridapGeosciences
-using GridapGeosciences.Distributed
-using GridapP4est
-using Test
-using GridapPETSc
+
 
 include("../convergence_tools.jl")
 
 dir = datadir("HodgeLaplacian_flat")
 !isdir(dir) && mkdir(dir)
 
-model0 = CartesianDiscreteModel((0, 1, 0, 1, 0, 1), (2, 2, 2))
-model1 = CartesianDiscreteModel((0, 1, 0, 1, 0, 1), (4, 4, 4))
-model2 = CartesianDiscreteModel((0, 1, 0, 1, 0, 1), (16, 16, 16))
-model3 = CartesianDiscreteModel((0, 1, 0, 1, 0, 1), (32, 32, 32))
+function dx(model::UnstructuredDiscreteModel{3,3})
+  1/( num_cells(model)^(1/3) )
+end
 
-models = [model3, model2, model1, model0]
+model0 = UnstructuredDiscreteModel( CartesianDiscreteModel((0, 1, 0, 1, 0, 1), (2, 2, 2)))
+model1 = UnstructuredDiscreteModel( CartesianDiscreteModel((0, 1, 0, 1, 0, 1), (4, 4, 4)))
+model2 = UnstructuredDiscreteModel( CartesianDiscreteModel((0, 1, 0, 1, 0, 1), (16, 16, 16)))
 
+models = [model2, model1, model0]
+ps = [0,1]
+ls = LUSolver()
 
 function uX(x)
-  VectorValue(x[1]^2,x[2]^2,x[3]^2)
+  VectorValue(x[1]^3,x[2]^2*x[1],x[1]*x[3]^2)
+  # VectorValue(x[1]^3,cos(2*π*x[1])*sin(2*π*x[1]),sin(2*π*x[1]))
 end
 
-divuX(x) = (∇⋅uX)(x)
-ccurl(x) = (∇×(∇×uX))(x)
-rhs(x) = ccurl(x) - gradient(divuX)(x)
 
-u(x) = x[1]^2
-f(x) = - Δ(u)(x)
+p_convergence_test([true],ps,models,flat_hodge_laplacian,dir,uX,ls,true)
+plot_convergence_from_saved(dir,"convergence",["u:", "s:"])
 
-panel_model = models[2]
-writevtk(Triangulation(panel_model),dir*"/rhs",
-           cellfields=["u"=>uX, "div"=>divuX, "rhs"=>rhs, "r1"=>f],append=false)
+function flat_hodge_laplacian(
+  model,
+  p_fe::Int,dir::String,uX::Function,ls=LUSolver(),return_vtk=false)
 
-ls = LUSolver()
-p_fe = 0
+  degree = 4*(p_fe + 1)
+  if p_fe == 0
+    degree = 8
+  end
+  @check degree > 0 "Zero quad!!"
+
+  ## finite element solver
+  Ω = Triangulation(model)
+  dΩ = Measure(Ω,2*degree)
+  Ω_error = Triangulation(model)
+  dΩ_error = Measure(Ω_error,4*degree)
+
+  tags = ["boundary"]
+  Γ = BoundaryTriangulation(model,tags=tags)
+  dΓ = Measure(Γ,2*degree)
+  nΓ = get_normal_vector(Γ)
 
 
-degree = 4*(p_fe + 1)
-if p_fe == 0
-  degree = 8
+  divuX(x) = (∇⋅uX)(x)
+  curluX(x) = (∇×uX)(x)
+  ccurl(x) = (∇×(∇×uX))(x)
+  rhs(x) = ccurl(x) - gradient(divuX)(x)
+
+  ## cellfields
+  rhs_cf = CellField(rhs,Ω)
+  u_cf = CellField(uX,Ω)
+  div_cf =  CellField(divuX,Ω)
+  sigma_cf = -div_cf
+  curlu_cf = CellField(curluX,Ω)
+
+
+  ## FE spaces
+  T = TestFESpace(Ω, ReferenceFE(lagrangian,Float64,p_fe+1); conformity=:H1)
+  S = TrialFESpace(T)
+
+  R = TestFESpace(Ω, ReferenceFE(nedelec,Float64,p_fe);conformity=:Hcurl)
+  H = TrialFESpace(R)
+
+
+  ### Multifield
+  X = MultiFieldFESpace([S,H])
+  Y = MultiFieldFESpace([T,R])
+
+  biform_x((s,u),(t,v)) = (
+                  ∫( (s*t)  )dΩ
+                - ∫( ∇(t)⋅u  )dΩ
+                + ∫( curl(u)⋅curl(v) )dΩ
+                + ∫( gradient(s)⋅v )dΩ
+                  )
+  liform_x((t,v)) = (
+                ∫( rhs_cf⋅v  )dΩ
+                + ∫( v⋅( curlu_cf ×nΓ) )dΓ
+                - ∫(( t*(u_cf⋅nΓ) )  )dΓ
+                  )
+
+
+  op = AffineFEOperator(biform_x,liform_x,X,Y)
+  sh,uh = solve(ls,op)
+
+
+  _e = sigma_cf - sh
+  el2_s = sqrt(sum(∫( (_e*_e)  )dΩ_error))
+
+  _e = uh - u_cf
+  el2_u = sqrt(sum(∫( (_e⋅_e)  )dΩ_error))
+
+  if return_vtk
+    cellfields =  ["u"=>u_cf,
+    "uh"=>uh,
+    "eu"=>uh-u_cf,
+    "sh"=>sh, "s"=>sigma_cf, "e"=>sh-sigma_cf,
+                  ]
+    writevtk(Ω,dir*"/flat_model_nref",
+            cellfields=cellfields,
+            append=false)
+  end
+
+  return el2_u, el2_s, false
+
 end
-
-## finite element solver
-Ω_panel = Triangulation(panel_model)
-dΩ = Measure(Ω_panel,2*degree)
-Ω_error = Triangulation(panel_model)
-dΩ_error = Measure(Ω_error,4*degree)
-
-## cellfields
-u_cf = CellField(uX,Ω_panel)
-div_cf = CellField(divuX,Ω_panel)
-rhs_cf = CellField(rhs,Ω_panel)
-sigma_cf = -div_cf
-
-
-tags = ["boundary"]
-
-## FE spaces
-T = TestFESpace(Ω_panel, ReferenceFE(lagrangian,Float64,p_fe+1); conformity=:H1,dirichlet_tags=tags)
-S = TrialFESpace(T,sigma_cf)
-
-R = TestFESpace(Ω_panel, ReferenceFE(nedelec,Float64,p_fe);conformity=:Hcurl,dirichlet_tags=tags)
-H = TrialFESpace(R,u_cf)
-
-X = MultiFieldFESpace([S,H])
-Y = MultiFieldFESpace([T,R])
-
-biform_s((s,u),(t,v)) = ∫( s*t )dΩ         - ∫( u⋅∇(t) )dΩ
-biform_u((s,u),(t,v)) = ∫( (∇×u)⋅(∇×v) )dΩ + ∫( v⋅∇(s) )dΩ
-biformX((s,u),(t,v)) = biform_s((s,u),(t,v)) + biform_u((s,u),(t,v))
-liformX((t,v)) = ∫( rhs⋅v )dΩ
-op = AffineFEOperator(biformX,liformX,X,Y)
-sh, uh = solve(ls,op)
-
-  # using LinearAlgebra
-  # A = get_matrix(op)
-  # evals = eigvals(Array(A))
-
-
-u_int = interpolate(u_cf,H)
-
-_e =  uh - u_int
-el2_u =  sqrt(sum(∫( _e⋅_e)dΩ_error))
-
-_e = sh - sigma_cf
-el2_s =  sqrt(sum(∫(  _e*_e )dΩ_error))
-
-
-cellfields = ["u"=>u_int,
-              "uh"=>uh,
-              "eu"=>u_int-uh,
-              "s"=>sigma_cf,
-              "sh"=>sh,
-              "es"=>sigma_cf-sh  ]
-
-### plot in 3D
-writevtk(Ω_panel,dir*"/flat_sol",
-        cellfields=cellfields,append=false)
---
+++ tests/HodgeLaplacian/preparejobs.jl
@@ -0,0 +1,135 @@
+
+using Mustache
+using DrWatson
+using DataStructures
+
+function jobname(args...)
+  s = savename(args...;connector="_",equals="_",sort=false,
+              ignores=(:dir,:return_vtk,:driver,:np))
+  s = replace(s,"return_vtk_" => "")
+  s = replace(s,"dir_" => "")
+  return s
+end
+
+function clean_params(d)
+  o = OrderedDict()
+  for k in keys(d)
+    if k == :theta
+      o[k] = ceil(Int, d[k]*100)
+    elseif isa(d[k],Real)
+      o[k] = Int(ceil(d[k]))
+    elseif isa(d[k],Symbol) || isa(d[k],String) || isa(d[k],Number)
+      o[k] = d[k]
+    elseif isa(d[k],Tuple)
+      o[k] = prod(d[k])
+    end
+  end
+  return o
+end
+
+# queue, cpumem, ncputask, ncpunode, walltime
+queue_info = Dict{Symbol,Any}(
+  :normal => ("normal",4,"4:00:00",48,1),
+  :hugemem => ("hugemem",30,"5:00:00",48,1),
+  :megamem => ("megamem",60,"12:00:00",48,1),
+  :saphire => ("normalsr",4.8,"2:00:00",104,4)
+)
+
+function jobdict(name,params,q)
+  np = params[:np] # number of processors
+
+  queue, cpumem, walltime, ncpunode, ncputask = queue_info[q]
+  project = haskey(ENV,"PROJECT") ? ENV["PROJECT"] : "zg98"
+
+  ntasks = prod(np)
+  ncpus  = ntasks * ncputask
+  mem = floor(Int, ncpus * cpumem)
+  # nnodes = ceil(Int, ncpus / ncpunode)
+  # ncpus = nnodes * ncpunode
+  # ntaskspernode = min(ncpunode ÷ ncputask,ntasks)
+
+  Dict(
+    "project" => project,
+    "q" => queue,
+    "o" => jobsdir*"/$(name).o",
+    "e" => jobsdir*"/$(name).e",
+    "walltime" => walltime,
+    # "nnodes" => nnodes,
+    "ncpus" => ncpus,
+    "mem" => "$(mem)G",
+    # "ntasks" => ntasks,
+    # "ncputask" => ncputask,
+    # "ntaskspernode" => ntaskspernode,
+    "name" => name,
+    "np" => np,            # Number of processors
+    "projectdir" => projectdir(),
+    "datadir"    => datadir(),
+    "simName"  => name,
+    "n" => params[:n], # refinement level
+    "order" => params[:order],
+    "dir" => params[:dir],
+    "return_vtk" => params[:return_vtk],
+    "driver" => params[:driver],
+  )
+end
+
+
+function generate_dictionaries(orders,nlevels,nprocs)
+  dicts = OrderedDict[]
+  for order in orders
+      for (nl,np) in zip(nlevels,nprocs)
+            aux = OrderedDict{Symbol,Any}(
+                        :n => nl, ## on cubed sphere meshes
+                        :order => order,
+                        :return_vtk => 1,
+                        :dir => dir,
+                        :driver => driver,
+                        :np => np,  # number of processors
+                      )
+            push!(dicts,aux)
+          end
+        end
+
+  return dicts
+end
+
+############################################
+
+jobsdir = projectdir("hodge_jobs")
+!isdir(jobsdir) && mkdir(jobsdir)
+
+dir = datadir("HodgeLaplacianConvergence")
+!isdir(dir) && mkdir(dir)
+
+wdir = projectdir("tests/HodgeLaplacian")
+driver = wdir*"/HodgeLaplacian.jl"
+
+
+queue = :megamem
+orders = [0, 1]
+nlevels = [1,2,3,4,5] # cubed sphere mesh
+nprocs  = [4,4,16,32,48]
+
+dicts = generate_dictionaries(orders,nlevels,nprocs)
+
+template = read(wdir*"/template.sh",String)
+
+for params in dicts
+  fparams = clean_params(params)
+  name = jobname(fparams)
+
+  # Generate job file
+  jobfile = projectdir(jobsdir*"/",name*".sh")
+  open(jobfile,"w") do io
+   render(io,template,jobdict(name,params,queue))
+  end
+end
+
+# submit job file
+for job in readdir(jobsdir)
+  println("Submitting job: ", job)
+  run(`qsub $jobsdir/$job`)
+end
+
+
+println("--DONE--")
