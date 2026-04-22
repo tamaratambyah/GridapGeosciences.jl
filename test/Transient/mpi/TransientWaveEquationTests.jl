b7978870427f08392c1d166027eac9658c872cc6
Tamara Tambyah <tamara.tambyah@monash.edu>
2026-04-22 17:01:33 +1000

    add missing refactorisation of transient tests

--- test/Transient/mpi/TransientWaveEquationTests.jl
+++ test/Transient/mpi/TransientWaveEquationTests.jl
@@ -7,8 +7,9 @@ MPI.Init()
 nprocs = prod(MPI.Comm_size(MPI.COMM_WORLD))
 ranks = distribute_with_mpi(LinearIndices((prod(nprocs),)))
 
+radius = 1.0
 ## Distributed model: 2D
-models = get_distributed_refined_models(ranks,nprocs,3)
+models = get_distributed_refined_models(ranks,nprocs,3,radius)
 TransientWaveEquationTests.main(models[1];_i_am_main=i_am_main(ranks))
 
 ### P4test model: 2D
