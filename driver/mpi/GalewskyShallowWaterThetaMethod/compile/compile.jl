922aad68932e586a232f815400e94cc2406ea713
Tamara Tambyah <tamara.tambyah@monash.edu>
2026-05-04 11:48:43 +1000

    remove old driver files

--- driver/mpi/GalewskyShallowWaterThetaMethod/compile/compile.jl
+++
@@ -1,4 +0,0 @@
-using PackageCompiler
-create_sysimage(:GalewskyShallowWaterThetaMethod,
-  sysimage_path=joinpath(@__DIR__,"..","GalewskyShallowWaterThetaMethod.so"),
-  precompile_execution_file=joinpath(@__DIR__,"warmup.jl"))
