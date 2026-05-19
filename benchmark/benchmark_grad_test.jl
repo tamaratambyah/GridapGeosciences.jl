3de312a508af58b8fb4e7f519900d6a77eb669fa
Tamara Tambyah <tamara.tambyah@monash.edu>
2026-05-19 10:05:53 +1000

    updates to benchmark

--- benchmark/benchmark_grad_test.jl
+++ benchmark/benchmark_grad_test.jl
@@ -8,7 +8,8 @@ using Gridap
 using BenchmarkTools
 using DrWatson
 using Gridap.Geometry
-using CountFlops
+# using CountFlops
+using GFlops
 
 include("single_panel_ambient_model.jl")
 include("analytic_metric.jl")
@@ -17,98 +18,120 @@ function bm_func(biform,trial,test)
   assemble_matrix(biform,trial,test)
 end
 
+## We have to overwrite this function for CountFlops to be compatible with
+## UnstructuredDiscreteModel. This is because it does not know that evaluate on
+## the iterator is type stable (and for some reason does a ccal).
+## In this function, provide the type to vals explicitily.
+## The length of this compressed array is one, so it should skip the for loop
+# function Gridap.Arrays._lazy_map_compressed(g::CompressedArray...)
+#   #vals = map(evaluate, map(gi->gi.values,g)...)
+#   n = length(first(g).values)
+#   v1 = evaluate(map(gi -> first(gi.values),g)...)
+#   vals = Vector{typeof(v1)}(undef,n)
+#   vals[1] = v1
+#   for i in 2:n
+#     vals[i] = evaluate(map(gi -> gi.values[i],g)...)
+#   end
+#   ptrs = first(g).ptrs
+#   Gridap.Arrays.CompressedArray(vals,ptrs)
+# end
+function Gridap.Arrays._lazy_map_compressed(g::CompressedArray...)
+  #vals = map(evaluate, map(gi->gi.values,g)...)
+  n = length(first(g).values)
+  v1 = evaluate(map(gi -> first(gi.values),g)...)
+  vals = Vector{typeof(v1)}(undef,n)
+  for i in 1:n
+    vals[i] = evaluate(map(gi -> gi.values[i],g)...)
+  end
+  ptrs = first(g).ptrs
+  Gridap.Arrays.CompressedArray(vals,ptrs)
+end
 
 
-nC = 2^1 ## num cells per panel
+function total_counts(flops::GFlops.Counter)
+  total = 0
+  for pn in propertynames(flops)
+      total += getproperty(flops, pn)
+  end
+  return total
+end
+
+biform(inv_g,sqrtg,dΩ) = (u,v) -> ∫( ( ∇(v)⋅ (inv_g⋅ ∇(u) ) )*sqrtg )dΩ
+
+
+nC = 2^2 ## num cells per panel
 radius = 1.0
 p_fe = 1
-panel_model = CartesianDiscreteModel((-π/4,π/4,-π/4,π/4),(nC,nC))
+_panel_model = CartesianDiscreteModel((-π/4,π/4,-π/4,π/4),(nC,nC))
+
+panel_model = _panel_model
 ambient_model = SingleAmbientDiscreteModel(panel_model,1.0)
 
 
-# function benchmark_gradgrad(ambient_model,panel_model,p_fe,dir,n=nref(ambient_model))
-  # println("nref = $n")
-  degree = 5*(p_fe+1)
+degree = 5*(p_fe+1)
 
-  ################################################################################
-  ########## Ambient model
-  ################################################################################
-  Ω_ambient = Triangulation(ambient_model)
-  dΩ_ambient = Measure(Ω_ambient,degree)
-  V_ambient = TestFESpace(Ω_ambient, ReferenceFE(lagrangian,Float64,p_fe); conformity=:H1)
-  U_ambient = TrialFESpace(V_ambient)
-  grad_grad_ambient(u,v) = ∫( ∇(u)⋅∇(v)   )dΩ_ambient
-
-  @benchmark bm_func($grad_grad_ambient,$U_ambient,$V_ambient)
-  t_ambient = @belapsed bm_func($grad_grad_ambient,$U_ambient,$V_ambient)
-  flops_ambient = @count_ops bm_func($grad_grad_ambient,$U_ambient,$V_ambient) ### FAILS!
+################################################################################
+########## Ambient model
+################################################################################
+Ω_ambient = Triangulation(ambient_model)
+dΩ_ambient = Measure(Ω_ambient,degree)
+V_ambient = TestFESpace(Ω_ambient, ReferenceFE(lagrangian,Float64,p_fe); conformity=:H1)
+U_ambient = TrialFESpace(V_ambient)
+# grad_grad_ambient(u,v) = ∫( ∇(u)⋅ ∇(v)   )dΩ_ambient
+grad_grad_ambient(u,v) = biform(identity_g,id_sqrtg,dΩ_ambient)(u,v)
 
 
-  ################################################################################
-  ########## Parametric model -- integrate in the physical domains (includes jacobians)
-  ################################################################################
-  Ω_panel = Triangulation(panel_model)
-  dΩ_panel = Measure(Ω_panel,degree)
-  V_panel = TestFESpace(Ω_panel, ReferenceFE(lagrangian,Float64,p_fe); conformity=:H1)
-  U_panel = TrialFESpace(V_panel)
-  grad_grad_panel(u,v) =  ∫( ( ∇(v)⋅ (inv_g⋅ ∇(u) ) )*_sqrtg )dΩ_panel
+bm_func(grad_grad_ambient,U_ambient,V_ambient) # warmup
 
-  @benchmark bm_func($grad_grad_panel,$U_panel,$V_panel)
-  t_panel = @belapsed bm_func($grad_grad_panel,$U_panel,$V_panel)
-  flops_panel = @count_ops bm_func($grad_grad_panel,$U_panel,$V_panel)
+# @benchmark bm_func($grad_grad_ambient,$U_ambient,$V_ambient)
+# t_ambient = @belapsed bm_func($grad_grad_ambient,$U_ambient,$V_ambient)
+ops_ambient = @count_ops bm_func($grad_grad_ambient,$U_ambient,$V_ambient)
+flops_ambient = @gflops bm_func($grad_grad_ambient,$U_ambient,$V_ambient)
 
 
-  ################################################################################
-  ########## Parametric model -- integrate in reference domain (no jacobians)
-  ########## Now the metric goes from the refee -> ambient space
-  ################################################################################
-  cmap = get_cell_map(panel_model.grid)
-  g_ref = lazy_map(Operation(inv_g),cmap)
-  g_ref_cf = Gridap.CellData.GenericCellField(g_ref,Ω_panel,ReferenceDomain())
+################################################################################
+########## Parametric model -- integrate in the physical domain (includes jacobians)
+################################################################################
+Ω_panel = Triangulation(panel_model)
+dΩ_panel = Measure(Ω_panel,degree)
+V_panel = TestFESpace(Ω_panel, ReferenceFE(lagrangian,Float64,p_fe); conformity=:H1)
+U_panel = TrialFESpace(V_panel)
+# grad_grad_panel(u,v) =  ∫( ( ∇(v)⋅ (inv_g⋅ ∇(u) ) )*_sqrtg )dΩ_panel
+grad_grad_panel(u,v) =  biform(inv_g,_sqrtg,dΩ_panel)(u,v)
 
-  meas_ref = lazy_map(Operation(_sqrtg),cmap)
-  meas_ref_cf = Gridap.CellData.GenericCellField(meas_ref,Ω_panel,ReferenceDomain())
+bm_func(grad_grad_panel,U_panel,V_panel) # warmup
 
-  quad = CellQuadrature(Ω_panel,degree;
-          data_domain_style=ReferenceDomain(),
-          integration_domain_style=ReferenceDomain())
-  dΩ_ref = Gridap.CellData.GenericMeasure(quad)
-
-  # grad_grad_panel_ref(u,v) =  ∫( ( ∇(v)⋅ ∇(u) )*meas_ref_cf )dΩ_ref
-  grad_grad_panel_ref(u,v) =  ∫( ( ∇(v)⋅ (g_ref_cf⋅ ∇(u) ) )*meas_ref_cf )dΩ_ref
-
-  @benchmark  bm_func($grad_grad_panel_ref,$U_panel,$V_panel)
-  t_ref = @belapsed bm_func($grad_grad_panel_ref,$U_panel,$V_panel)
-  flops_ref = @count_ops bm_func($grad_grad_panel_ref,$U_panel,$V_panel)
-
-  ;
-  # ### convergence output for DrWatson
-  # dir_convergence = dir
-  # ( !isdir(dir_convergence)) && mkpath(dir_convergence)
-  # output = @strdict n t_panel t_ambient alloc_panel alloc_ambient p_fe  t_panel_ref alloc_panel_ref
-  # safesave(datadir(dir_convergence, ("benchmark_nref$(n)_p$(p_fe).jld2")), output)
-
-  # println("Time: Ambient $t_ambient, Parametric $t_panel, Reference $t_panel_ref")
-  # println("Allocats: Ambient $alloc_ambient, Parametric $alloc_panel, Reference $alloc_panel_ref")
-
-  # return t_ambient,alloc_ambient, t_panel, alloc_panel
+# @benchmark bm_func($grad_grad_panel,$U_panel,$V_panel)
+# t_panel = @belapsed bm_func($grad_grad_panel,$U_panel,$V_panel)
+ops_panel = @count_ops bm_func($grad_grad_panel,$U_panel,$V_panel)
+flops_panel = @gflops bm_func($grad_grad_panel,$U_panel,$V_panel)
 
 
 
-# p_fe = 1
-# radius = 1
-# n_ref_lvls = 5
+################################################################################
+########## Parametric model -- integrate in reference domain (no jacobians)
+########## Now the metric goes from the refee -> ambient space
+################################################################################
+cmap = get_cell_map(panel_model.grid)
+g_ref = lazy_map(Operation(inv_g),cmap)
+g_ref_cf = Gridap.CellData.GenericCellField(g_ref,Ω_panel,ReferenceDomain())
 
+meas_ref = lazy_map(Operation(_sqrtg),cmap)
+meas_ref_cf = Gridap.CellData.GenericCellField(meas_ref,Ω_panel,ReferenceDomain())
 
-# include("single_panel_ambient_model.jl")
-# nC = 2^1
-#   single_panel_model = CartesianDiscreteModel((-π/4,π/4,-π/4,π/4),(nC,nC))
-#   single_ambient_model = SingleAmbientDiscreteModel(single_panel_model,radius)
+quad = CellQuadrature(Ω_panel,degree;
+        data_domain_style=ReferenceDomain(),
+        integration_domain_style=ReferenceDomain())
+dΩ_ref = Gridap.CellData.GenericMeasure(quad)
 
-# dir = datadir("grad_grad_single_panel_flops")
-# for i in collect(1:n_ref_lvls)
-#   nC = 2^i
-#   single_panel_model = CartesianDiscreteModel((-π/4,π/4,-π/4,π/4),(nC,nC))
-#   single_ambient_model = SingleAmbientDiscreteModel(single_panel_model,radius)
-#   benchmark_gradgrad(single_ambient_model,single_panel_model,p_fe,dir,i)
-# end
+# grad_grad_panel_ref(u,v) =  ∫( ( ∇(v)⋅ (g_ref_cf⋅ ∇(u) ) )*meas_ref_cf )dΩ_ref
+grad_grad_panel_ref(u,v) =  biform(g_ref_cf,meas_ref_cf,dΩ_ref)(u,v)
+
+# @benchmark  bm_func($grad_grad_panel_ref,$U_panel,$V_panel)
+# t_ref = @belapsed bm_func($grad_grad_panel_ref,$U_panel,$V_panel)
+ops_ref = @count_ops bm_func($grad_grad_panel_ref,$U_panel,$V_panel)
+flops_ref = @gflops bm_func($grad_grad_panel_ref,$U_panel,$V_panel)
+
+total_counts(ops_ambient)
+total_counts(ops_panel)
+total_counts(ops_ref)
--- benchmark/single_panel_ambient_model.jl
+++ benchmark/single_panel_ambient_model.jl
@@ -50,3 +50,42 @@ function SingleAmbientDiscreteModel(panel_model::UnstructuredDiscreteModel,radiu
   UnstructuredDiscreteModel(ambient_grid,ambient_topo,labels)
 
 end
+
+
+
+function SinglePanelDiscreteModel(panel_model::UnstructuredDiscreteModel)
+
+  panel_grid = get_grid(panel_model)
+  panel_topo = get_grid_topology(panel_model)
+  labels = get_face_labeling(panel_model)
+
+  ## map: reffe -> alpha,beta
+  cmap = get_cell_map(panel_grid)
+
+  ref_pts = get_cell_ref_coordinates(panel_grid)
+  ambient_cell_coords = lazy_map(evaluate,cmap,ref_pts)
+
+  eT = eltype(testitem(ambient_cell_coords))
+  panel_nodes = Gridap.Geometry.num_nodes(panel_grid)
+  ambient_nodes = fill(zero(eT),panel_nodes)
+  # Now create proper nodes from the cell-wise array of coords
+  # We can do this for the ambient model because the nodes are defined uniquely
+  # in ambient space
+  cell_to_nodes = get_cell_node_ids(panel_grid)
+  Gridap.FESpaces._free_and_dirichlet_values_fill!(
+    ambient_nodes, eT[],
+    array_cache(ambient_cell_coords),
+    array_cache(cell_to_nodes),
+    ambient_cell_coords,
+    cell_to_nodes,
+    eachindex(cell_to_nodes)
+  )
+
+  ## the ambient_grid has the bespoke panel_2_ambient_cmap
+  ambient_grid = Gridap.Geometry.UnstructuredGrid(ambient_nodes,get_cell_node_ids(panel_grid),get_reffes(panel_grid),get_cell_type(panel_grid),OrientationStyle(panel_grid),
+                      nothing,cmap)
+  ambient_topo = UnstructuredGridTopology(ambient_nodes,get_cell_node_ids(panel_grid),get_cell_type(panel_topo),get_polytopes(panel_topo),OrientationStyle(panel_topo))
+
+  UnstructuredDiscreteModel(ambient_grid,ambient_topo,labels)
+
+end
